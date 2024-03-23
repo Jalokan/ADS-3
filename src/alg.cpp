817bc2538fa1a0db32aeace871f7b4c2e5f04bfd
Jalokan <150829310+Jalokan@users.noreply.github.com>
2024-03-23 21:33:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,19 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+	int leftborder = 0;
+	int rightborder = size - 1;
+	while (leftborder <= rightborder) {
+		int center = (leftborder + rightborder) / 2;
+		if (*(arr + center) == value) {
+			return center;
+		}
+		else if (*(arr + center) < value) {
+			leftborder = center + 1;
+		}
+		else {
+			rightborder = center - 1;
+		}
+	}
+	return 0; 
 }
